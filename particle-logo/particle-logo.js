faee6d3c4525564a903a51b50c5eaf73f88cb0fb
Miaolegemie <1942037006@qq.com>
2017-10-19 21:38:39 +0800

    add EASE

--- particle-logo/particle-logo.js
+++ particle-logo/particle-logo.js
@@ -1,7 +1,187 @@
 const EASE = {
-	easeInOutExpo: function(e, a, g, f) {
-		return g * (-Math.pow(2, -10 * e / f) + 1) + a
-	}
+  linear(e, a, g, f) {
+    return g * e / f + a
+  },
+  easeInOutQuad(e, a, g, f) {
+    e /= f / 2
+    if (e < 1) {
+      return g / 2 * e * e + a
+    }
+    e--
+    return -g / 2 * (e * (e - 2) - 1) + a
+  },
+  easeOutQuad(e, a, g, f) {
+    e /= f
+    return -g * e * (e - 2) + a
+  },
+  easeOutQuad(e, a, g, f) {
+    e /= f
+    return -g * e * (e - 2) + a
+  },
+  easeInCubic(e, a, g, f) {
+    e /= f
+    return g * e * e * e + a
+  },
+  easeOutCubic(e, a, g, f) {
+    e /= f
+    e--
+    return g * (e * e * e + 1) + a
+  },
+  easeInOutCubic(e, a, g, f) {
+    e /= f / 2
+    if (e < 1) {
+      return g / 2 * e * e * e + a
+    }
+    e -= 2
+    return g / 2 * (e * e * e + 2) + a
+  },
+  easeInQuart(e, a, g, f) {
+    e /= f
+    return g * e * e * e * e + a
+  },
+  easeOutQuart(e, a, g, f) {
+    e /= f
+    e--
+    return -g * (e * e * e * e - 1) + a
+  },
+  easeInOutQuart(e, a, g, f) {
+    e /= f / 2
+    if (e < 1) {
+      return g / 2 * e * e * e * e + a
+    }
+    e -= 2
+    return -g / 2 * (e * e * e * e - 2) + a
+  },
+  easeInQuint(e, a, g, f) {
+    e /= f
+    return g * e * e * e * e * e + a
+  },
+  easeOutQuint(e, a, g, f) {
+    e /= f
+    e--
+    return g * (e * e * e * e * e + 1) + a
+  },
+  easeInOutQuint(e, a, g, f) {
+    e /= f / 2
+    if (e < 1) {
+      return g / 2 * e * e * e * e * e + a
+    }
+    e -= 2
+    return g / 2 * (e * e * e * e * e + 2) + a
+  },
+  easeInSine(e, a, g, f) {
+    return -g * Math.cos(e / f * (Math.PI / 2)) + g + a
+  },
+  easeOutSine(e, a, g, f) {
+    return g * Math.sin(e / f * (Math.PI / 2)) + a
+  },
+  easeInOutSine(e, a, g, f) {
+    return -g / 2 * (Math.cos(Math.PI * e / f) - 1) + a
+  },
+  easeInExpo(e, a, g, f) {
+    return g * Math.pow(2, 10 * (e / f - 1)) + a
+  },
+  easeOutExpo(e, a, g, f) {
+    return g * (-Math.pow(2, -10 * e / f) + 1) + a
+  },
+  easeInOutExpo(e, a, g, f) {
+    return g * (-Math.pow(2, -10 * e / f) + 1) + a
+  },
+  easeInCirc(e, a, g, f) {
+    e /= f
+    return -g * (Math.sqrt(1 - e * e) - 1) + a
+  },
+  easeOutCirc(e, a, g, f) {
+    e /= f
+    e--
+    return g * Math.sqrt(1 - e * e) + a
+  },
+  easeInOutCirc(e, a, g, f) {
+    e /= f / 2
+    if (e < 1) {
+      return -g / 2 * (Math.sqrt(1 - e * e) - 1) + a
+    }
+    e -= 2
+    return g / 2 * (Math.sqrt(1 - e * e) + 1) + a
+  },
+  easeInOutElastic(g, e, k, j, f, i) {
+    if (g == 0) {
+      return e
+    }
+    if ((g /= j / 2) == 2) {
+      return e + k
+    }
+    if (!i) {
+      i = j * (0.3 * 1.5)
+    }
+    if (!f || f < Math.abs(k)) {
+      f = k
+      var h = i / 4
+    } else {
+      var h = i / (2 * Math.PI) * Math.asin(k / f)
+    }
+    if (g < 1) {
+      return -0.5 * (f * Math.pow(2, 10 * (g -= 1)) * Math.sin((g * j - h) * (2 * Math.PI) / i)) + e
+    }
+    return f * Math.pow(2, -10 * (g -= 1)) * Math.sin((g * j - h) * (2 * Math.PI) / i) * 0.5 + k + e
+  },
+  easeInElastic(g, e, k, j, f, i) {
+    if (g == 0) {
+      return e
+    }
+    if ((g /= j) == 1) {
+      return e + k
+    }
+    if (!i) {
+      i = j * 0.3
+    }
+    if (!f || f < Math.abs(k)) {
+      f = k
+      var h = i / 4
+    } else {
+      var h = i / (2 * Math.PI) * Math.asin(k / f)
+    }
+    return -(f * Math.pow(2, 10 * (g -= 1)) * Math.sin((g * j - h) * (2 * Math.PI) / i)) + e
+  },
+  easeOutElastic(g, e, k, j, f, i) {
+    if (g == 0) {
+      return e
+    }
+    if ((g /= j) == 1) {
+      return e + k
+    }
+    if (!i) {
+      i = j * 0.3
+    }
+    if (!f || f < Math.abs(k)) {
+      f = k
+      var h = i / 4
+    } else {
+      var h = i / (2 * Math.PI) * Math.asin(k / f)
+    }
+    return (f * Math.pow(2, -10 * g) * Math.sin((g * j - h) * (2 * Math.PI) / i) + k + e)
+  },
+  easeInOutBack(e, a, h, g, f) {
+    if (f == undefined) {
+      f = 1.70158
+    }
+    if ((e /= g / 2) < 1) {
+      return h / 2 * (e * e * (((f *= (1.525)) + 1) * e - f)) + a
+    }
+    return h / 2 * ((e -= 2) * e * (((f *= (1.525)) + 1) * e + f) + 2) + a
+  },
+  easeInBack(e, a, h, g, f) {
+    if (f == undefined) {
+      f = 1.70158
+    }
+    return h * (e /= g) * e * ((f + 1) * e - f) + a
+  },
+  easeOutBack(e, a, h, g, f) {
+    if (f == undefined) {
+      f = 1.70158
+    }
+    return h * ((e = e / g - 1) * e * ((f + 1) * e + f) + 1) + a
+  }
 }
 class ImageToParticles {
 	// { canvas, image, cols, rows, startX, startY, imageX, imageY, imageHeight, imageWidth, duration, interval, ease }
